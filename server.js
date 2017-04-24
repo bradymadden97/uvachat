80f8bd1de16c3c36b685c9f96f6fa0d1901206d7
Brady Madden <bradymadden97@gmail.com>
2017-04-24 17:40:25 -0400

    update to work online

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('user connected');
+	
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
+	
+server.listen(process.env.PORT || 3000, function(){
+	console.log("Server listening.");
+});
