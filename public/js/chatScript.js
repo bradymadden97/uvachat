2726c3876196fbd7ad78cab7969a8584bd46ceec
Brady Madden <bradymadden97@gmail.com>
2017-04-24 22:32:10 -0400

    moved chat to right, made style changes with text indent and time height

--- public/js/chatScript.js
+++ public/js/chatScript.js
@@ -28,9 +28,9 @@
 				min = '0' + min;
 			msg = msg.replace(/((((http|ftp|https):\/\/)|(www.))[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?)/g, "<a target='_blank' href='$1' >$1</a>");
 			if(currentspeaker == upcomingspeaker)
-				$("#messages_box").append($('<li>').append($('<span class="txt">').append('<span class="indenttextchat">'+msg+'</span>')).append($('<span class="time">').append(hr+':'+min+' '+ampm)));
+				$("#messages_box").append($('<li>').append($('<span class="txt">').append('<div class="indenttextchat">'+msg+'</div>')).append($('<span class="time">').append(hr+':'+min+' '+ampm)));
 			else{
 				currentspeaker = upcomingspeaker;
-				$("#messages_box").append($('<li>').append($('<span class="txt">').append("<b>"+upcomingspeaker+":</b><br>"+'<span class="indenttextchat">'+msg+'</span>')).append($('<span class="time">').append(hr+':'+min+' '+ampm)));
+				$("#messages_box").append($('<li>').append($('<span class="txt">').append("<b>"+upcomingspeaker+":</b><br>"+'<div class="indenttextchat">'+msg+'</div>')).append($('<span class="time">').append(hr+':'+min+' '+ampm)));
 			}
 		});
